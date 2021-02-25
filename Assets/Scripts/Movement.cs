f0a65b3f65e6a94fc1b9bd0e4759d4194d5434d6
Michael Watts <wattsjmichael@gmail.com>
2021-02-25 10:46:38 -0800

    Adds Gravity, and rotation

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+  Rigidbody rb;
+  [SerializeField] float mainThrust = 100f;
+  [SerializeField] float rotation = 50f;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    ProcessThrust();
+    ProcessRotation();
+
+  }
+  void ProcessThrust()
+  {
+    if (Input.GetKey(KeyCode.W))
     {
-        
+      rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
     }
+  }
+
+  void ProcessRotation()
+  {
+    if (Input.GetKey(KeyCode.A))
+    {
+      ApplyRotation(rotation);
+    }
+    else if (Input.GetKey(KeyCode.D))
+    {
+      ApplyRotation(-(rotation));
+    }
+  }
+
+  private void ApplyRotation(float rotationThisFrame)
+  {
+    rb.freezeRotation = true; //Freezing rotation to manually rotate
+    transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+    rb.freezeRotation = false;
+  }
 }
+
